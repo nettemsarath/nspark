b8ca7fb1aebf928de6b8f514d3f3573742666b5b
nettemsarath <nettemsarath1@gmail.com>
2025-12-24 09:39:16 +0530

    build: added cname

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      base: '/nspark/',
+      base: '/',
       plugins: [react()],
       resolve: {
         alias: {
